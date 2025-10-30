c5affe5aa98bcaba7519042e74a36ef54e8ed99a
zhzhzlajava <zhzhzlajava@gmail.com>
2025-10-30 19:54:48 +0900

    [ADD] 파일 경로를 위한 config 파일 추가

---
+++ fortrip/src/main/java/com/fortrip/com/app/common/config/WebConfig.java
@@ -0,0 +1,20 @@
+package com.fortrip.com.app.common.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Value("${file.upload-path}")
+    private String uploadPath;
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        // /uploads/** 요청 → 실제 파일 경로
+        registry.addResourceHandler("/uploads/**")
+                .addResourceLocations("file:" + uploadPath);
+    }
+}
